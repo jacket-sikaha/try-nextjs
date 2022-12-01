3e1379b1805a568395b08d74966f087d27e738de
sikaha <987555458@qq.com>
2022-12-01 15:51:12 +0800

    一级导航/嵌套的二级导航设置

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -1,12 +1,29 @@
 import { useRouter } from "next/router";
 
 function ClientsPage() {
-  const { query } = useRouter();
+  const { query, push } = useRouter();
   console.log("ClientsPage", query);
+  function gotonextrouter() {
+    // load data ...
+    push(`/clients/${query.id}/test`);
+  }
+  function gotonextrouter2() {
+    // load data ...
+    push({
+      pathname: "/clients/[id]/[clientid]",
+      query: {
+        id: query.id,
+        clientid: "test2",
+      },
+    });
+  }
   return (
     <>
       <div>ClientsPage</div>
       <div>这是第一层动态路由</div>
+      <button onClick={gotonextrouter}>go to next router</button>
+      <br />
+      <button onClick={gotonextrouter2}>go to next router2</button>
     </>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,19 @@
+import Link from "next/link";
 import styles from "../styles/Home.module.css";
-
 export default function Home() {
   return (
     <div className={styles.container}>
       <h1>sikara</h1>
+      <ul>
+        <li>
+          <Link href="/portfolio">portfolio</Link>
+        </li>
+        <li>
+          <Link replace href="/clients">
+            clients
+          </Link>
+        </li>
+      </ul>
     </div>
   );
 }
