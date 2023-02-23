1595e8a5d3b3c34f72be9dfc77b9e4d985e15008
sikara <987555458@qq.com>
2023-02-23 22:59:34 +0800

    测试得mongodb版本不影响

--- aa.js
+++ aa.js
@@ -4,7 +4,7 @@ async function name(params) {
   const a = await connectToDatabase();
   const movies = a.db("sample_mflix").collection("movies");
   // Query for a movie that has the title 'Back to the Future'
-  const query = { title: "111111111111Back to the Future" };
+  const query = { title: "asdasdasd to the Future" };
   const movie = await movies.insertOne(query);
   console.log(movie);
   await a.close();
--- pages/api/user/change-password.js
+++ pages/api/user/change-password.js
@@ -1,4 +1,4 @@
-import { getSession } from "next-auth/client";
+import { getSession } from "next-auth/react";
 
 import { hashPassword, verifyPassword } from "../../../lib/auth";
 import { usersCollection, update } from "../../../lib/db";
